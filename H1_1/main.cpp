370f591c244d7b1a1008bdd9dd02babd14af1413
Joona <joona.louhisalmi@gmail.com>
2025-11-07 22:46:37 +0200

    Siivottu kansiorakenne – poistettu ylimääräiset kopiot

---
+++ H1_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cout << "Anna nimesi: ";
+    cin >> name;
+    cout << "Anna ikäsi: ";
+    cin >> age;
+    cout << "terve " << name << " olet siis " << age << " -vuotias" << endl;
+    cin >>age;
+    return 0;
+}
